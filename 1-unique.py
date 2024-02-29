21ab0cffb8f02f16cd50e49b5779c976d0bb9c4e
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2024-02-29 12:13:57 +0000

    update expected results for exercise 1

--- 1-unique.py
+++ 1-unique.py
@@ -11,5 +11,5 @@ def unique_elements(data: list):
 print(unique_elements([1, 2, 2, 3, 4, 4, 5])) # [1, 2, 3, 4, 5]
 print(unique_elements([1, 1, 1, 1, 1, 1, 1])) # [1]
 print(unique_elements([1, 2, 3, 4, 5, 6, 7])) # [1, 2, 3, 4, 5, 6, 7]
-print(unique_elements(['cat', 'dog', 'hamster', 'cat', 'dog', 'cat', 'dog'])) # ['cat', 'dog']
+print(unique_elements(['cat', 'dog', 'hamster', 'cat', 'dog', 'cat', 'dog'])) # ['cat', 'dog', 'hamster']
 print(unique_elements([])) # []
